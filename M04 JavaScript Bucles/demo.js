4f56fbcd1f9b30a7ac3b9dfe9da7abafcb37a8b0
DAbarboza <bardanii7@gmail.com>
2023-10-19 00:10:24 -0300

    act

---
+++ M04 JavaScript Bucles/demo.js
@@ -0,0 +1,12 @@
+//! BUCEL FOR
+
+// for (var i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+// for (var i = 10; i >= 0; i++) {
+//   console.log(i);
+// }
+
+function encontrarVocalA(string) {
+  for (var i = 0; i < string.length; i++) {}
+}
