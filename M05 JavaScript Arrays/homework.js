e7d1c21e7d04984df12605c968659957a58c2eaf
DABarboza <bardanii7@gmail.com>
2023-10-21 12:58:51 -0300

    act

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -85,6 +85,9 @@ function promedioResultadosTest(resultadosTest) {
   // El parámetro "resultadosTest" es un arreglo de números.
   // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
    // Tu código:
+   for (i = 0; i < resultadosTest.length; i++){
+      
+   }
    
 }
 
